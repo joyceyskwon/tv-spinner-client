518bacb3b6d98631b454624c498aad4b3e8aebf4
github username <github email address>
2019-02-08 17:19:49 -0500

    added login and signup forms

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -2,10 +2,32 @@ import React from 'react'
 
 export default class LogIn extends React.Component {
 
+  state = {
+    loginClicked: false
+  }
+
+  showLoginForm = e => {
+    this.setState({
+      loginClicked: !this.state.loginClicked
+    })
+  }
+
   render() {
     return (
       <div>
-        
+        <button
+          onClick={() => this.showLoginForm()}
+        >
+          Login
+        </button>
+
+        {this.state.loginClicked &&
+          <form>
+          <input type="text" name="name" placeholder="Username" />
+          <input type="password" name="password" placeholder="Password" />
+          <input type="Submit" value="Login" />
+          </form>
+        }
       </div>
     )
   }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,10 +2,32 @@ import React from 'react'
 
 export default class SignUp extends React.Component {
 
+  state = {
+    signupClicked: false
+  }
+
+  showSignUpForm = e => {
+    this.setState({
+      signupClicked: !this.state.signupClicked
+    })
+  }
+
   render() {
     return (
       <div>
-      
+        <button
+          onClick={() => this.showSignUpForm()}
+        >
+          Sign Up
+        </button>
+        
+        {this.state.signupClicked &&
+          <form>
+            <input type="text" name="name" placeholder="Username" />
+            <input type="password" name="password" placeholder="Password" />
+            <input type="Submit" value="Sign Up" />
+          </form>
+        }
       </div>
     )
   }
