ac6f04321be7b083af73576ae6aa62a0be03863d
github username <github email address>
2019-02-08 17:23:11 -0500

    before I push on git hub

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import ShowContainer from './components/ShowContainer'
 import Nav from './components/Nav'
 
 export default class App extends Component {
+
+  state = {
+    loggedin: false
+  }
+
   render() {
     return (
       <div className="App">
         <Nav />
+
         <ShowContainer />
       </div>
     )
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -12,6 +12,7 @@ export default class LogIn extends React.Component {
     })
   }
 
+
   render() {
     return (
       <div>
